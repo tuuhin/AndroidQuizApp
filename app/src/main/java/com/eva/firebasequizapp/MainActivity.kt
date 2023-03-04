ba185d166959cc276cca0622992ea3a0d045123b
tuuhin <tuhinbhowmick2513@gmail.com>
2023-03-05 01:37:46 +0530

    Initialized project firebase quiz app

---
+++ app/src/main/java/com/eva/firebasequizapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.eva.firebasequizapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.eva.firebasequizapp.ui.theme.FirebaseQuizAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirebaseQuizAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
